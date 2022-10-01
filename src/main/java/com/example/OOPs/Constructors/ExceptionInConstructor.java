86d3a8e8d160d7c981bae6b2d45df1c0a293765b
SHADOW13325 <saumitra13325@gmail.com>
2022-10-01 15:14:13 +0530

    added comment

--- src/main/java/com/example/OOPs/Constructors/ExceptionInConstructor.java
+++ src/main/java/com/example/OOPs/Constructors/ExceptionInConstructor.java
@@ -28,4 +28,4 @@ class ChildWithException extends ParentWithException{
 
 // If parent class constructor throws any checked exception, compulsory child class constructor
 // should throw the same checked exception or its parent otherwise compile time error "Unreported exception"
-// We can't write super() in try-catch block as first line in constructor must be suoer() or this()
+// We can't write super() in try-catch block as first line in constructor must be super() or this()
