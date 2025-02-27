de1d43d931a4a08d93178415bcc160b190a985c4
Saumitra Chauhan <saumitradeveloper@gmail.com>
2025-02-27 08:03:15 +0530

    added adapter pattern

---
+++ src/main/java/com/example/DesignPatterns/Behavioral/AdapterPattern.java
@@ -0,0 +1,52 @@
+package com.example.DesignPatterns.Behavioral;
+
+public class AdapterPattern {
+    public static void main(String[] args) {
+
+        WeightMachine weightMachine = new WeightMachineForBabies(35);
+        WeightAdapter weightAdapter = new WeightAdapterImpl(weightMachine);
+        System.out.println(weightAdapter.getWeightInKg());
+    }
+}
+
+
+interface WeightAdapter {
+
+    double getWeightInKg ();
+}
+
+class WeightAdapterImpl implements WeightAdapter {
+
+    WeightMachine weightMachine;
+
+    public WeightAdapterImpl(WeightMachine weightMachine) {
+        this.weightMachine = weightMachine;
+    }
+
+    @Override
+    public double getWeightInKg() {
+
+        double weightInPound = weightMachine.getWeightInPound();
+
+        return weightInPound * .45;
+    }
+}
+
+
+interface WeightMachine {
+    double getWeightInPound();
+}
+
+class WeightMachineForBabies implements WeightMachine {
+
+    double babyWeight;
+
+    public WeightMachineForBabies (double babyWeight) {
+        this.babyWeight = babyWeight;
+    }
+
+    @Override
+    public double getWeightInPound() {
+        return this.babyWeight;
+    }
+}
