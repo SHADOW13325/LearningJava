fe4c9e65397d24b0a1e6ae16f27bf06390b0d59c
Saumitra Chauhan <saumitradeveloper@gmail.com>
2025-04-15 21:52:00 +0530

    added builder pattern

--- src/main/java/com/example/DesignPatterns/Creational/BuilderPattern.java
+++ src/main/java/com/example/DesignPatterns/Creational/BuilderPattern.java
@@ -1,7 +1,149 @@
 package com.example.DesignPatterns.Creational;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BuilderPattern {
     public static void main(String[] args) {
+        Director directorObj1 = new Director(new EngineeringStudentBuilder());
+        Director directorObj2 = new Director(new MBAStudentBuilder());
 
+        Student engineerStudent = directorObj1.createStudent();
+        Student mbaStudent =directorObj2.createStudent();
+
+        System.out.println(engineerStudent.toString());
+        System.out.println(mbaStudent.toString());
+
+    }
+}
+
+class Student {
+    int rollNumber;
+    int age;
+    String name;
+    String fatherName;
+    String motherName;
+    List<String> subjects;
+
+    public Student(StudentBuilder builder){
+        this.rollNumber = builder.rollNumber;
+        this.age = builder.age;
+        this.name = builder.name;
+        this.fatherName = builder.fatherName;
+        this.motherName = builder.motherName;
+        this.subjects = builder.subjects;
+    }
+
+    public String toString(){
+        return ""+ " roll number: " + this.rollNumber +
+                " age: " + this.age +
+                " name: " + this.name +
+                " father name: " + this.fatherName +
+                " mother name: " + this.motherName +
+                " subjects: " + subjects.get(0) + "," + subjects.get(1) + "," + subjects.get(2);
+    }
+}
+
+// Abstract Builder
+abstract class StudentBuilder {
+
+    int rollNumber;
+    int age;
+    String name;
+    String fatherName;
+    String motherName;
+    List<String> subjects;
+
+    public StudentBuilder setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+        return this;
+    }
+
+    public StudentBuilder setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public StudentBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public StudentBuilder setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+        return this;
+    }
+
+    public StudentBuilder setMotherName(String motherName) {
+        this.motherName = motherName;
+        return this;
+    }
+
+    abstract public StudentBuilder setSubjects();
+
+    // getResult()
+    public Student build() {
+        return new Student(this);
+    }
+
+}
+
+// Director
+class Director {
+
+    StudentBuilder studentBuilder;
+
+    Director(StudentBuilder studentBuilder){
+        this.studentBuilder = studentBuilder;
+    }
+
+    public Student createStudent(){
+
+        if(studentBuilder instanceof EngineeringStudentBuilder){
+            return createEngineeringStudent();
+        }
+        else if(studentBuilder instanceof MBAStudentBuilder){
+            return createMBAStudent();
+        }
+        return null;
+    }
+
+    public void changeBuilder(StudentBuilder studentBuilder) {
+        this.studentBuilder = studentBuilder;
+    }
+
+    private Student createEngineeringStudent(){
+        return studentBuilder.setRollNumber(1).setAge(22).setName("sj").setSubjects().build();
+    }
+
+    private Student createMBAStudent(){
+        return studentBuilder.setRollNumber(2).setAge(24).setName("sj").setFatherName("MyFatherName")
+                .setMotherName("MyMotherName").setSubjects().build();
+    }
+}
+
+class EngineeringStudentBuilder extends StudentBuilder{
+
+    @Override
+    public StudentBuilder setSubjects() {
+        List<String> subs = new ArrayList<>();
+        subs.add("DSA");
+        subs.add("OS");
+        subs.add("Computer Architecture");
+        this.subjects = subs;
+        return this;
+    }
+}
+
+class MBAStudentBuilder extends StudentBuilder{
+
+    @Override
+    public StudentBuilder setSubjects() {
+        List<String> subs = new ArrayList<>();
+        subs.add("Micro Economics");
+        subs.add("Business Studies");
+        subs.add("Operations Management");
+        this.subjects = subs;
+        return this;
     }
 }
